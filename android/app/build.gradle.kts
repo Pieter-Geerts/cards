65c084266f5166f858fb4e565f572489e9cc2087
Pieter Geerts <pgeerts@gmail.com>
2025-05-20 16:02:24 +0200

    pipeline

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,9 +23,13 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
     kotlinOptions {
         jvmTarget = "17"
+    }
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
     }
 
     defaultConfig {
